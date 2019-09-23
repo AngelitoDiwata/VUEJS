e1a19aae1dd4159d54327b5cee031f3159b325f1
Angelito <diwataangelito@gmail.com>
2019-09-23 14:11:35 +0800

    DONE: NUXT Calculator app!

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="container">
+    <center><img id="imgMain" src="https://miro.medium.com/max/600/1*Z3QcBB__mvWCqood2fbiBQ.png"></center>
+    <br>
+    <p><center>Calculator</center></p>
+    <br>
+    <br>
+    <div id="displayInputs">
+       <div class="input-field col s26">
+          <input ref="resultScreen"  id="numInput" type="number" class="validate" :value="currentValue" @input="setValue">
+        </div>
+        <a id="delBtn" @click="delNumber" class="btn-floating btn-large waves-effect waves-light blue"><i class="material-icons">keyboard_arrow_left</i></a>
+    </div>
       <numKeys />
   </div>
 </template>
@@ -10,10 +21,56 @@ import numKeys from '../components/numKeys'
 export default {
   components: {
     numKeys
-  }
+  },
+  methods: {
+      setValue(){
+           this.$store.commit('setNewVal', this.$refs.resultScreen.value);
+      },
+
+      delNumber(){
+          let currentNum = this.$refs.resultScreen.value;
+          let deletedNumVal = currentNum.slice(0, -1);
+          this.$store.commit('setNewVal', deletedNumVal);
+      }
+  },
+    computed: {
+        currentValue: function(){
+         return this.$store.getters.getResultValue
+        }
+    },
 }
 </script>
 
 <style scoped>
+
+.container{
+  padding: 100px;
+    margin: 0 auto;
+    width: 500px;
+    height: auto;
+}
+
+#imgMain{
+  height: 80px;
+  width: 200px;
+}
+
+#displayInputs{
+  display: grid;
+  grid-template-areas: 'input delbtn';
+  grid-template-columns: 85% 20%;
+  width: 100%;
+  margin: 0 auto;
+}
+
+#numInput{
+  grid-area: input;
+}
+
+#delBtn{
+  text-align: center;
+  grid-area: delbtn;
+}
+
 
 </style>
--- store/index.js
+++ store/index.js
@@ -1,40 +1,110 @@
 ﻿export const state = () => ({
-    result: 0
+    result: '',
+    holdValue: '',
+    operator: ''
 })
 
 export const mutations = {
-    addition(state, mSum) {
-        state.result = mSum
+    addition(state) {
+        state.operator = '+';
+        state.holdValue = state.result;
+        state.result = '';
     },
-    subtraction(state, mDiff) {
-        state.result = mDiff
+    subtraction(state) {
+        state.operator = '-'
+        state.holdValue = state.result;
+        state.result = '';
     },
-    multiplication(state, mProd) {
-        state.result = mProd
+    multiplication(state) {
+        state.operator = '*';
+        state.holdValue = state.result;
+        state.result = '';
     },
-    division(state, mQuot) {
-        state.result = mQuot
+    division(state) {
+        state.operator = '/';
+        state.holdValue = state.result;
+        state.result = '';
+    },
+    btnPress(state, newVal) {
+        state.result += newVal;
+    },
+    setNewVal(state, newVal) {
+        state.result = newVal;
     }
 }
-export const actions = {
-    addition(context, mCurrentVal) {
-        let mHold = state.result
-        let mSum = mCurrentVal + mHold
-        context.commit('addition', mSum)
-    },
-    subtraction(context, mCurrentVal) {
-        let mHold = state.result
-        let mDiff = mCurrentVal - mHold
-        context.commit('subtraction', mDiff)
-    },
-    multiplication(context, mCurrentVal) {
-        let mHold = state.result
-        let mProd = mCurrentVal * mHold
-        context.commit('multiplication', mProd)
-    },
-    division(context, mCurrentVal) {
-        let mHold = state.result
-        let mQuot = mCurrentVal / mHold
-        context.commit('division', mQuot)
+
+export const getters = {
+    getResultValue(state) {
+        return state.result;
     }
+}
+
+
+export const actions = {
+    addition(context) {
+        context.commit('addition');
+    },
+    subtraction(context) {
+        context.commit('subtraction');
+    },
+    multiplication(context) {
+        context.commit('multiplication');
+    },
+    division(context) {
+        context.commit('division');
+    },
+    btn1(context) {
+        let newVal = '1';
+        context.commit('btnPress', newVal);
+    },
+    btn2(context) {
+        let newVal = '2';
+        context.commit('btnPress', newVal);
+    },
+    btn3(context) {
+        let newVal = '3';
+        context.commit('btnPress', newVal);
+    },
+    btn4(context) {
+        let newVal = '4';
+        context.commit('btnPress', newVal);
+    },
+    btn5(context) {
+        let newVal = '5';
+        context.commit('btnPress', newVal);
+    },
+    btn6(context) {
+        let newVal = '6';
+        context.commit('btnPress', newVal);
+    },
+    btn7(context) {
+        let newVal = '7';
+        context.commit('btnPress', newVal);
+    },
+    btn8(context) {
+        let newVal = '8';
+        context.commit('btnPress', newVal);
+    },
+    btn9(context) {
+        let newVal = '9';
+        context.commit('btnPress', newVal);
+    },
+    btn0(context) {
+        let newVal = '0';
+        context.commit('btnPress', newVal);
+    },
+    btnE(context) {
+        let newVal = 0;
+        if (this.state.operator === '+') {
+            newVal = parseInt(this.state.result) + parseInt(this.state.holdValue);
+        } else if (this.state.operator === '-') {
+            newVal = parseInt(this.state.holdValue) - parseInt(this.state.result);
+        } else if (this.state.operator === '*') {
+            newVal = parseInt(this.state.result) * parseInt(this.state.holdValue);
+        } else if (this.state.operator === '/') {
+            newVal = parseInt(this.state.result) / parseInt(this.state.holdValue);
+        }
+        context.commit('setNewVal', newVal);
+    },
+
 }
